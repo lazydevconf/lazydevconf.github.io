f5b2460475f5d0dbd7fb6c3e0e43fa96e29acea8
woo-yu <hjwoou@gmail.com>
2025-04-02 10:12:57 +0900

    불필요 파일 제거 및 service worker 제거

--- src/index.js
+++ src/index.js
@@ -5,23 +5,15 @@ import { AppProvider } from "./appContext";
 // https://create-react-app.dev/docs/adding-bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { NavermapsProvider } from 'react-naver-maps';
+import { NavermapsProvider } from "react-naver-maps";
 
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-      <AppProvider>
-        <NavermapsProvider
-          ncpClientId="j4o164tqxe"
-        >
-          <App />
-        </NavermapsProvider>
-      </AppProvider>
+  <AppProvider>
+    <NavermapsProvider ncpClientId="j4o164tqxe">
+      <App />
+    </NavermapsProvider>
+  </AppProvider>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
